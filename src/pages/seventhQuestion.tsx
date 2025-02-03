3df2ab612232be1e58652712f5e19ae26f4cfed0
Rener-cardoso <rener_cardoso@hotmail.com>
2025-02-03 05:13:47 -0300

    fix: fixing seventhQuestion

--- src/pages/seventhQuestion.tsx
+++ src/pages/seventhQuestion.tsx
@@ -8,27 +8,27 @@ interface CarrotProps {
 export function SeventhQuestion() {
   function getMaxValue(carrotTypes: CarrotProps[], capacity: number) {
     carrotTypes.sort((a, b) => (b.price / b.kg) - (a.price / a.kg));
-  
-    let totalValue = 0;
-    let remainingCapacity = capacity;
-  
+    
+    let maxValue = 0;
+    
     for (const carrot of carrotTypes) {
-      if (remainingCapacity === 0) break;
-  
-      if (carrot.kg <= remainingCapacity) {
-        totalValue += carrot.price;
-        remainingCapacity -= carrot.kg;
-      } else {
-        totalValue += (carrot.price / carrot.kg) * remainingCapacity;
-        remainingCapacity = 0;
-      }
+        if (capacity === 0) break;
+        
+        const amount = Math.min(carrot.kg, capacity);
+        maxValue += (amount / carrot.kg) * carrot.price;
+        capacity -= amount;
     }
-  
-    return totalValue;
-  }
+    
+    return maxValue;
+}
 
   useEffect(() => {
-    const carrotTypes = [{kg: 5, price: 100}, {kg: 7, price: 150}, {kg: 3, price: 70}]
+    const carrotTypes = [
+      {kg: 5, price: 100}, 
+      {kg: 7, price: 150}, 
+      {kg: 3, price: 70},
+    ]
+
     const capacity = 36
 
     const result = getMaxValue(carrotTypes, capacity);
